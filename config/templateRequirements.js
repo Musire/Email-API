84a1fd1b2c99ebc8c3f7b5985b5d90320e96edba
musire <mcmusire@gmail.com>
2025-08-18 18:12:11 -0600

    updating the requirements

--- config/templateRequirements.js
+++ config/templateRequirements.js
@@ -2,7 +2,7 @@ const requiredFields = {
   "welcome": ["name", "link"],
   "verify": ["name", "verificationLink"],
   "password-reset": ["username", "resetLink"],
-  "store-order": ['contact', 'items']
+  "store-order": ['contact', 'cartItems']
 };
 
 module.exports = requiredFields
